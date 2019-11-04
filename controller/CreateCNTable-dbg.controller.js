29bd3db59271c35ebe2e27c2a7dedeaf5f212f2b
Halomoan <halomoan@gmail.com>
2019-11-04 09:49:26 +0800

    home commit

---
+++ controller/CreateCNTable-dbg.controller.js
@@ -0,0 +1,111 @@
+sap.ui.define([
+	"fin/re/conmgmts1/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	'sap/ui/core/Fragment',
+	'sap/ui/model/Filter'
+], function(BaseController,JSONModel,Fragment,Filter) {
+	"use strict";
+
+	return BaseController.extend("fin.re.conmgmts1.controller.CreateCNTable", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf fin.re.conmgmts1.view.CreateCNTable
+		 */
+		onInit: function() {
+			var oModel = new JSONModel("/model/CNModel.json");
+			this.getView().setModel(oModel,"CNModel");
+		},
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf fin.re.conmgmts1.view.CreateCNTable
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf fin.re.conmgmts1.view.CreateCNTable
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf fin.re.conmgmts1.view.CreateCNTable
+		 */
+		//	onExit: function() {
+		//
+		//	}
+		onDelete: function() {
+			var oTreeTable = this.byId("createCNTable");
+			console.log(oTreeTable.getSelectedIndices());
+			
+		},
+		onCollapseAll: function() {
+			var oTreeTable = this.byId("createCNTable");
+			oTreeTable.collapseAll();
+		},
+		
+		
+		onExpandFirstLevel: function() {
+			var oTreeTable = this.byId("createCNTable");
+			oTreeTable.expandToLevel(1);
+		},
+		
+		onAddUnit: function(oEvent){
+			
+			
+			
+			if (!this._oSelectUnit) {
+				this._oSelectUnit = sap.ui.xmlfragment("fin.re.conmgmts1.fragment.selectUnit", this);
+				this._oSelectUnit.setModel(this.getView().getModel());
+			}
+			
+			this.getView().addDependent(this._oSelectUnit);
+			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oSelectUnit);
+			this._oSelectUnit.open();
+
+		/*	var oTreeTable = this.byId("createCNTable");
+			var rowIdx = oEvent.getSource().getParent().getIndex();
+			
+			
+			var oModel = oTreeTable.getBinding("rows").getModel();
+			
+			var oContext = oTreeTable.getContextByIndex(rowIdx);
+			var oData = oContext.getProperty();
+			
+			var oRow = {"isParent": false,
+						 "reobjnr" : "1",
+						 "reunit"  : "#02-02",
+						"unitsize": "1,500.00 SF"
+						
+						};
+			oData.rows.push(oRow);
+			oModel.refresh();
+			oTreeTable.expand(rowIdx);*/
+				
+			
+		},
+		onCloseDialog: function(){
+			this._oSelectUnit.close();
+		},
+		onUnitSearch: function(oEvent){
+			var sValue = oEvent.getParameter("value");
+			
+		
+			var oFilter1 = new Filter("Xmetxt", sap.ui.model.FilterOperator.Contains, sValue);
+			var oFilter2 = new Filter("Smenr", sap.ui.model.FilterOperator.Contains, sValue);
+			var oBinding = oEvent.getSource().getBinding("items");
+			oBinding.filter([oFilter1]);
+		}
+
+	});
+
+});
